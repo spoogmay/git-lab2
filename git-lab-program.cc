a952eff5fa7b939c603c29857bb420492abf6e93
Spoogmay <fs207118@ohio.edu>
2020-05-15 00:30:10 -0400

    Hello Git!!

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Spoogmay>
+ *        Date: <14 may ,2020>
+ * Description: hw practice
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+ cout << "Hello Git!!" << endl;
+    return 0;
+}// main
